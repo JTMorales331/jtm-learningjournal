e500ffabd420b30e71c992821aa07bbd72d9d278
Ter[r]y <johnterence.morales@benilde.edu.ph>
2024-10-23 22:01:36 +0800

    initial commit. already fixed scss styles organization and added Footer and Header component files

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+// import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return(
+    <header>
+      <div className="container">
+        <h1>My Traveling Journal</h1>
+        <nav>
+          <a>Home</a>
+          <a>About Me</a>
+        </nav>
+      </div>
+    </header>
+  )
+}
