c1d5c8d22a8b5bb2bb60bfb1dac8682b29420fe9
Ter[r]y <johnterence.morales@benilde.edu.ph>
2024-10-30 16:41:08 +0800

    added a content-wrapper. this pushes the footer to the outmost bottom, maximizing the viewport height

--- src/components/Layouts/BaseLayout.jsx
+++ src/components/Layouts/BaseLayout.jsx
@@ -4,12 +4,12 @@ import Footer from '../Footer'
 
 export default function BaseLayout() {
   return (
-    <>
+    <div className='content-wrapper'>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   )
 }
